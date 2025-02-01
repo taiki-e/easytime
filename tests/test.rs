0790705d6c39ac30e2c4a701869cc5ed766e8d02
Taiki Endo <te316e89@gmail.com>
2025-02-01 11:29:37 +0900

    tests: Update size test

--- tests/test.rs
+++ tests/test.rs
@@ -7,7 +7,7 @@ use easytime::*;
 // Test the size of public types. This is not intended to keep a specific size and
 // is intended to be used only as a help in optimization.
 #[test]
-#[cfg_attr(any(not(target_pointer_width = "64"), miri), ignore)] // We set -Z randomize-layout for Miri.
+#[cfg_attr(any(not(target_pointer_width = "64"), miri, careful), ignore)] // We set -Z randomize-layout for Miri/cargo-careful.
 fn size() {
     assert_eq!(mem::size_of::<Duration>(), 16);
     assert_eq!(mem::size_of::<TryFromTimeError>(), 0);
